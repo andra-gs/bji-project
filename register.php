71f5bd0a048197f094e8d3bb83d96c2d9db2e0e9
SINT13 <galihrizkia22@gmail.com>
2025-12-02 16:31:56 +0700

    Update register.php

--- register.php
+++ register.php
@@ -43,9 +43,65 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <input type="password" name="password" placeholder="Password" required><br>
     <select name="kelas" required>
         <option value="">-- Pilih Kelas --</option>
-        <option value="X">X</option>
-        <option value="XI">XI</option>
-        <option value="XII">XII</option>
+        <option value="">-- Pilih Kelas --</option>
+
+<option value="X tjkt 1">X tjkt 1</option>
+<option value="X tjkt 2">X tjkt 2</option>
+<option value="X tjkt 3">X tjkt 3</option>
+<option value="X tjkt 4">X tjkt 4</option>
+
+<option value="X tav 1">X tav 1</option>
+<option value="X tav 2">X tav 2</option>
+<option value="X tav 3">X tav 3</option>
+
+<option value="X tsm 1">X tsm 1</option>
+<option value="X tsm 2">X tsm 2</option>
+<option value="X tsm 3">X tsm 3</option>
+
+<option value="X ak 1">X ak 1</option>
+<option value="X ak 2">X ak 2</option>
+<option value="X ak 3">X ak 3</option>
+
+<option value="X mp 1">X mp 1</option>
+<option value="X mp 2">X mp 2</option>
+<option value="X mp 3">X mp 3</option>
+<option value="X mp 4">X mp 4</option>
+
+<option value="XI tjkt 1">XI tjkt 1</option>
+<option value="XI tjkt 2">XI tjkt 2</option>
+<option value="XI tjkt 3">XI tjkt 3</option>
+<option value="XI tjkt 4">XI tjkt 4</option>
+
+<option value="XI tav 1">XI tav 1</option>
+<option value="XI tav 2">XI tav 2</option>
+<option value="XI tav 3">XI tav 3</option>
+
+<option value="XI tsm 1">XI tsm 1</option>
+<option value="XI tsm 2">XI tsm 2</option>
+<option value="XI tsm 3">XI tsm 3</option>
+
+<option value="XI ak 1">XI ak 1</option>
+<option value="XI ak 2">XI ak 2</option>
+<option value="XI ak 3">XI ak 3</option>
+
+<option value="XI mp 1">XI mp 1</option>
+<option value="XI mp 2">XI mp 2</option>
+<option value="XI mp 3">XI mp 3</option>
+<option value="XI mp 4">XI mp 4</option>
+
+<option value="XII tkjt 1">XII tkjt 1</option>
+<option value="XII tkjt 2">XII tkjt 2</option>
+<option value="XII tkjt 3">XII tkjt 3</option>
+<option value="XII tkjt 4">XII tkjt 4</option>
+
+<option value="XII tav 1">XII tav 1</option>
+<option value="XII tav 2">XII tav 2</option>
+<option value="XII tav 3">XII tav 3</option>
+
+<option value="XII tsm 1">XII tsm 1</option>
+<option value="XII tsm 2">XII tsm 2</option>
+<option value="XII tsm 3">XII tsm 3</option>
+
     </select><br>
     <button type="submit">Daftar</button>
 </form>
